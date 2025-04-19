cc8c1e8ca88e4273e230d261c8c9863e0f64e223
Daniel Arevalo <danielarevalo@MacBook-Pro-de-Yoryiany.local>
2025-04-19 15:36:12 -0300

    initial commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+              cloud_name: 'arevaloweb',
+              api_key: '699291333499611',
+              api_secret: 'qru9lRSkxTnazI4OWr_EwxBIC7k',
+              secure: true,
+            },
+            actionOptions: {
+              uploadStream: {
+                folder: 'strapi',
+              },
+              delete: {},
+            },
+        },
+    },
+});
